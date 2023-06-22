3174ca86b26dc11abc7512c01c11223be41456b2
Jaime Matheus <mjaimeg33@gmail.com>
2023-06-22 16:04:54 -0500

    UC3_PARTE02_MATHEUS

--- UC3_EdsonAlvarado/UC3_EdsonAlvarado/urls.py
+++ UC3_EdsonAlvarado/UC3_EdsonAlvarado/urls.py
@@ -21,5 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.index, name = "index"),
     path('index/',views.index, name = "index"),
- 
+    path('inicios/',views.inicios, name = "inicios"),
 ]
--- UC3_EdsonAlvarado/miapp/views.py
+++ UC3_EdsonAlvarado/miapp/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render, HttpResponse, redirect
 
 
+
+
 def index(request):
     return render(request,'index.html')
 
+def inicios(request):
+    cursos = ['Gestion de base de datos', 'Dinamica de sistemas', 'LP3', 'Teoria general de sistemas', 'Algoritmo de computacion grafica', 'Gestion de procesos', 'Legislacion informatica']
+    return render(request, 'inicios.html', {'cursos': cursos})
